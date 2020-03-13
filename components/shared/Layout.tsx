36684de6a3c1fde709b882afce623342d8b07cfb
cryptoji <kocherginji@gmail.com>
2020-03-13 23:37:53 +0300

    ADD: Page layout component

---
+++ components/shared/Layout.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import Head from 'next/head'
+import styles from './Layout.module.scss'
+import Header from './Header'
+
+export default function Layout(props) {
+  return (
+    <div className={styles.Wrapper}>
+      <Head>
+        <title>OZEEX</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          key="viewport"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Header />
+      <main>{props.children}</main>
+    </div>
+  )
+}
