d4eddf63077e510e788b0d8cb1e06e422a2ebb51
cryptoji <kocherginji@gmail.com>
2020-03-14 11:01:27 +0300

    FIX: Global update of Card component

--- modules/+index/components/Card.tsx
+++ modules/+index/components/Card.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { MutableRefObject, useContext, useEffect, useMemo, useRef, useState } from 'react'
 import styles from './Card.module.scss'
-import { IItem } from '../../../pages/api/search'
+import { IItem } from '../interfaces'
+import { IndexContext } from '../../../pages'
 import CardTags from './CardTags'
 import CardSlider from './CardSlider'
 
@@ -9,30 +10,84 @@ interface IVideoCardProps {
 }
 
 const Card: React.FC<IVideoCardProps> = ({ item }) => {
-  const [isOpenedDetails, setIsOpenedDetails] = useState(false)
+  const indexContext = useContext(IndexContext)
+  const [contentHeight, setContentHeight] = useState(0)
+  const isCollapsedDetails = useMemo(
+    () => indexContext.collapsedCardId === item.id,
+    [indexContext.collapsedCardId]
+  )
 
-  function showDetails() {}
+  // Elements refs
+  const descriptionEl = useRef(null)
+  const tagsEl = React.createRef<HTMLDivElement>()
+
+  useEffect(() => {
+    if (isCollapsedDetails) {
+      const totalHeight = [descriptionEl, tagsEl].reduce(
+        (memo: number, el: MutableRefObject<HTMLElement>) => {
+          memo += el.current.offsetHeight + el.current.offsetTop
+          return memo
+        },
+        0
+      )
+
+      setContentHeight(totalHeight)
+    } else {
+      setContentHeight(0)
+    }
+  }, [isCollapsedDetails])
+
+  const cardClasses = `${styles.Wrapper} ${
+    isCollapsedDetails ? styles.DetailsCollapsed : ''
+  }`
+
+  function collapseDetails() {
+    indexContext.setCollapsedCardId(item.id)
+  }
 
   return (
-    <article className={styles.Wrapper}>
-      <figure className={styles.ImageContainer}>
-        <CardSlider pictures={item.picture} />
+    <article className={cardClasses}>
+      <div className={styles.ImageContainer}>
+        <CardSlider itemId={item.id} pictures={item.picture} />
 
-        <div className={styles.Meta}>
-          <small>{item.added}</small>
-          <small>{item.duration}</small>
+        <ul className={styles.Meta}>
+          <li className={styles.Added}>
+            <strong>Added:</strong> <span>{item.added}</span>
+          </li>
+          <li className={styles.Duration}>
+            <strong>Duration:</strong> <span>{item.duration}</span>
+          </li>
+          <li className={styles.Source}>
+            <strong>Source:</strong> <span>{item.source}</span>
+          </li>
+        </ul>
+      </div>
+
+      <div
+        className={styles.ContentWrapper}
+        style={{
+          height: contentHeight ? contentHeight : 'auto',
+        }}
+      >
+        <div className={styles.Content}>
+          <p ref={descriptionEl} className={styles.Description}>
+            {isCollapsedDetails && <strong>Description</strong>}{' '}
+            {item.description}
+          </p>
+
+          <CardTags
+            ref={tagsEl}
+            tags={item.tag}
+            isCollapsedDetails={isCollapsedDetails}
+          />
         </div>
+      </div>
 
-        <figcaption className={styles.Description}>
-          {item.description}
-        </figcaption>
-      </figure>
-
-      <CardTags tags={item.tag} />
-
-      <button className={styles.MoreInfo} onClick={showDetails}>
-        More info
-      </button>
+      {!isCollapsedDetails && (
+        <button className={styles.MoreInfo} onClick={collapseDetails}>
+          More info
+        </button>
+      )}
     </article>
   )
 }
