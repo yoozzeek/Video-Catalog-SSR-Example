e2e68d9971aad3d2259c2060b573d184f0dd3dfd
cryptoji <kocherginji@gmail.com>
2020-03-14 08:46:16 +0300

    FIX: Remove API endpoint because in doesn't work in prod (need investigation)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,38 @@
 import * as React from 'react'
 import { GetStaticProps } from 'next'
-import fetch from 'isomorphic-unfetch'
 import styles from './index.module.scss'
+import mockData from '../public/results.json'
 import Layout from '../shared/components/Layout'
 import IndexMeta from '../modules/+index/components/Meta'
-import { ISearchData } from './api/search'
 import Card from '../modules/+index/components/Card'
+
+/**
+ * Data interfaces
+ */
+export interface IItemPicture {
+  id: number
+  path: string
+}
+
+export interface IItem {
+  id: number
+  source: string
+  description: string
+  added: string
+  duration: string
+  picture: IItemPicture[]
+  tag: string[]
+  pornstar: string[]
+  webcam: string[]
+}
+
+export interface ISearchData {
+  info: {
+    total: number
+    search_term: string
+  }
+  item: IItem[]
+}
 
 /**
  * Index page component
@@ -25,13 +52,11 @@ const Index: React.FC<ISearchData> = props => {
 }
 
 /**
- * Fetch search result on server side
+ * Fetch mock data on server side
  */
 export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch('http://localhost:3000/api/search')
-  const data = await res.json()
   return {
-    props: data,
+    props: mockData,
   }
 }
 
