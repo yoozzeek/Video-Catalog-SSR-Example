20e95462c63d1132d6cc5265d52c41f0f3a532ee
cryptoji <kocherginji@gmail.com>
2020-03-14 11:01:36 +0300

    FIX: Update Index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,46 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import { GetStaticProps } from 'next'
 import styles from './index.module.scss'
 import mockData from '../public/results.json'
+import { ISearchData } from '../modules/+index/interfaces'
 import Layout from '../shared/components/Layout'
 import IndexMeta from '../modules/+index/components/Meta'
 import Card from '../modules/+index/components/Card'
 
-/**
- * Data interfaces
- */
-export interface IItemPicture {
-  id: number
-  path: string
-}
-
-export interface IItem {
-  id: number
-  source: string
-  description: string
-  added: string
-  duration: string
-  picture: IItemPicture[]
-  tag: string[]
-  pornstar: string[]
-  webcam: string[]
-}
-
-export interface ISearchData {
-  info: {
-    total: number
-    search_term: string
-  }
-  item: IItem[]
-}
+export const IndexContext = React.createContext({
+  collapsedCardId: null,
+  launchedSlideShowId: null,
+  setCollapsedCardId: newId => newId,
+  setLaunchedSlideShowId: newId => newId,
+})
 
 /**
  * Index page component
  */
 const Index: React.FC<ISearchData> = props => {
+  // Index page global context
+  // available in any nested component
+  const [collapsedCardId, setCollapsedCardId] = useState(null)
+  const [launchedSlideShowId, setLaunchedSlideShowId] = useState(null)
+
   return (
     <Layout>
       <IndexMeta term={props.info.search_term} total={props.info.total} />
 
-      <section className={styles.VideosList}>
-        {props.item.map((item, index) => (
-          <Card key={index} item={item} />
-        ))}
-      </section>
+      <IndexContext.Provider
+        value={{
+          collapsedCardId,
+          launchedSlideShowId,
+          setCollapsedCardId,
+          setLaunchedSlideShowId,
+        }}
+      >
+        <section className={styles.VideosList}>
+          {props.item.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
+        </section>
+      </IndexContext.Provider>
     </Layout>
   )
 }
