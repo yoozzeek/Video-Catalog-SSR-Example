30ca938307272b5b62e1c4d837a3e97b21a000b0
cryptoji <kocherginji@gmail.com>
2020-03-19 22:27:30 +0300

    FIX: Style of index page updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 import { GetStaticProps } from 'next'
 import styles from './index.module.scss'
 import mockData from '../public/results.json'
-import { ISearchData } from '../modules/+index/interfaces'
-import Layout from '../shared/components/Layout'
-import IndexMeta from '../modules/+index/components/Meta'
-import Card from '../modules/+index/components/Card'
+import { ISearchData } from '../modules/index/interfaces'
+import Layout from '../components/Layout'
+import IndexMeta from '../modules/index/components/Meta'
+import Card from '../modules/index/components/Card'
 
 export const IndexContext = React.createContext({
   collapsedCardId: null,
@@ -26,7 +26,6 @@ const Index: React.FC<ISearchData> = props => {
   return (
     <Layout>
       <IndexMeta term={props.info.search_term} total={props.info.total} />
-
       <IndexContext.Provider
         value={{
           collapsedCardId,
