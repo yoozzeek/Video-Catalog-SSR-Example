e8685f23c889f9e5fd0dfdae390795355bc559dc
cryptoji <kocherginji@gmail.com>
2020-03-19 22:25:51 +0300

    FIX: Remove shared folder (all common components there are in components folder)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -20,7 +20,18 @@ export default function Layout(props) {
       </Head>
 
       <Header />
+
       <main>{props.children}</main>
+
+      <footer className={styles.Footer}>
+        <p>
+          Disclaimer: All models are 18 years or older. All porn galleries and
+          linksto sex videos are provided by 3rd parties. We have no control
+          over thecontent of these pages. We take no responsibility for the
+          content on anywebsite which we link to, please use your own discretion
+          while surfing theporn links. And we are proudly labeled with the RTA.
+        </p>
+      </footer>
     </div>
   )
 }
