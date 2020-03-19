233a4959cf40cf32b1638f2cb869b4220ba8e805
cryptoji <kocherginji@gmail.com>
2020-03-19 22:26:59 +0300

    FIX: Update CardTags component

---
+++ modules/index/components/CardTags.tsx
@@ -0,0 +1,44 @@
+import React, { MutableRefObject } from 'react'
+import styles from './CardTags.module.scss'
+
+interface ICardTagsProps {
+  tags: string[]
+  label?: string
+  color?: 'primary' | 'success' | 'secondary'
+  isHidden?: boolean
+  isCollapsedDetails: boolean
+}
+
+const CardTags = React.forwardRef<HTMLDivElement, ICardTagsProps>(
+  (
+    {
+      tags,
+      isCollapsedDetails,
+      label = 'Tags:',
+      color = 'secondary',
+      isHidden = false,
+    },
+    ref: MutableRefObject<HTMLDivElement>
+  ) => {
+    const classes = `${styles.Wrapper} ${isCollapsedDetails ? styles.Wrap : ''}`
+
+    return (
+      <div
+        ref={ref}
+        className={classes}
+        style={isHidden ? { display: 'none' } : {}}
+      >
+        {isCollapsedDetails && (
+          <strong className={styles.Label}>{label}</strong>
+        )}
+        {tags.map((tag, index) => (
+          <span key={index} className={`${styles.Tag} ${styles[color]}`}>
+            {tag}
+          </span>
+        ))}
+      </div>
+    )
+  }
+)
+
+export default CardTags
